e2a29fb0e240964785094a0f3a1fcb8e2aef4a5e
jaceanderson120 <jace.anderson120@gmail.com>
2025-05-29 12:26:34 -0400

    Add files via upload

---
+++ Workflow/Workflow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workflow
+//
+//  Created by Jace Anderson on 8/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! I am adding this text to practice checking out to a new branch and committing.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Jace Anderson on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
